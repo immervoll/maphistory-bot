496cef1840af7d4bb28d6b98217b87be5c7b0487
d0cplay <docplay.emp@gmail.com>
2022-06-23 22:08:59 +0200

    introduced selfupdating perma embed

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import a2s
 import logging
 import os
 from datetime import datetime
+import time
+import pickle
+
 logging.basicConfig(level=logging.INFO)
 
 if is_docker():
@@ -19,7 +22,6 @@ else:
         TOKEN = DATA["TOKEN"]
         PREFIX = DATA["PREFIX"]
         SERVERADDRESS = (DATA["SERVER"]["IP"], DATA["SERVER"]["PORT"])
-        MESSAGEID = DATA["MESSAGEID"]
 
 INTERVAL = 600.0  # Interval in seconds to check the server
 FIELPATH = "./history.txt"  # Path to the file to save the history
@@ -27,6 +29,7 @@ FIELPATH = "./history.txt"  # Path to the file to save the history
 
 bot = commands.Bot(PREFIX)
 last_map: str = ""
+
 
 @bot.event
 async def on_ready():
@@ -45,14 +48,14 @@ async def queryServer():
 
         query = a2s.info(SERVERADDRESS)
         current_map = query.map_name
-        
+
         if not current_map == last_map:
             with open(FIELPATH, "a") as file:
                 file.write(f"{current_map}\n")
                 logging.log(logging.INFO, f"Saved map to File")
                 logging.log(logging.INFO, f"Current map: {current_map}")
                 logging.log(logging.INFO, f"Last map: {last_map}")
-            
+
             last_map = current_map
         else:
             logging.log(logging.INFO, f"Current map: {current_map}")
@@ -65,14 +68,30 @@ async def queryServer():
         logging.log(logging.ERROR, f"Couldnt complete a2s query. {e}")
 
     finally:
-        embed=discord.Embed(title="Map History", description="", color=0xff0000)
-        embed.set_author(name="Map History Bot", url="https://github.com/immervoll/maphistory-bot", icon_url="icon")
-        embed.add_field(name="Current Map", value=f"{current_map}", inline=False)
-        embed.add_field(name="Last 10 Maps", value=f"{last_map}", inline=False)
-        from datetime import datetime
-        embed.set_footer(text=f"Last update: {datetime.now()}")
-        
-        await ctx.send(embed=embed)
+        try:
+            await update_staticEmbed()
+        except:
+            pass
+
+
+async def update_staticEmbed():
+    with open('embed.pickle', 'rb') as embedPickleFile:
+        staticEmbedInfo = pickle.load(embedPickleFile)
+
+    message = await bot.get_channel(staticEmbedInfo[0]).fetch_message(staticEmbedInfo[1])
+
+    last_10_maps = "".join(get_last_10_maps())
+    embed = discord.Embed(title="Map History", description="", color=0xff0000)
+    embed.set_author(name="Map History Bot",
+                     url="https://github.com/immervoll/maphistory-bot")
+    embed.add_field(name="Current Map", value="".join(
+        get_current_map()), inline=False)
+    embed.add_field(name="Last 10 Maps", value=f"{last_10_maps}", inline=False)
+    embed.add_field(name="Last refresh", value=f"<t:{int(time.time())}:R>")
+    embed.set_footer(text="by immervoll")
+    embed.timestamp = datetime.now()
+    await message.edit(embed=embed)
+
 
 def get_last_10_maps():
     with open(FIELPATH, "r") as file:
@@ -97,4 +116,33 @@ async def history(ctx: commands.Context):
     embed.set_footer(text="by immervoll")
     await ctx.send(f"{ctx.author.mention} here is the servers map history", embed=embed)
 
+
+@bot.command(name="setup",
+             aliases=["set"],
+             usage="!setup <channel> - sets up a the channel to host the perma history info",
+             description="sets up a channel to host a self updating embed showing the server history")
+@commands.guild_only()
+@commands.has_permissions(administrator=True)
+@commands.cooldown(1, 2, commands.BucketType.member)
+async def setup(ctx: commands.Context, *, channelID: int):
+
+    channel = discord.utils.get(bot.get_all_channels(), id=channelID)
+
+    last_10_maps = "".join(get_last_10_maps())
+    embed = discord.Embed(title="Map History", description="", color=0xff0000)
+    embed.set_author(name="Map History Bot",
+                     url="https://github.com/immervoll/maphistory-bot")
+    embed.add_field(name="Current Map", value="".join(
+        get_current_map()), inline=False)
+    embed.add_field(name="Last 10 Maps", value=f"{last_10_maps}", inline=False)
+    embed.add_field(name="Last refresh", value=f"<t:{int(time.time())}:R>")
+    embed.set_footer(text="by immervoll")
+    embed.timestamp = datetime.now()
+    message = await channel.send(embed=embed)
+
+    staticEmbedInfo = [channel.id, message.id]
+    with open('embed.pickle', 'wb') as embedPickleFile:
+        pickle.dump(staticEmbedInfo, embedPickleFile)
+
+    await ctx.send(f"Set up embed in {channel.mention}")
 bot.run(TOKEN)
