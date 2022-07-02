4a9e4b1e3b961996ff51582de720c7cddbf3e490
Immervoll <docplay.emp@gmail.com>
2022-07-02 15:35:48 +0200

    Invert "Last 10 Maps" Fixes #23

--- history.py
+++ history.py
@@ -9,32 +9,32 @@ class History(object):
     server_name: str
     current_map: str
     last_map: str
-    last_10_maps: list
+    last_maps: list
     last_update: int
 
     def __init__(self):
         if os.path.exists("./history.pickle"):
             with open("./history.pickle", "rb") as historyPickleFile:
-                self.last_10_maps = pickle.load(historyPickleFile)
-                self.current_map = self.last_10_maps[-1][0]
+                self.last_maps = pickle.load(historyPickleFile)
+                self.current_map = self.last_maps[-1][0]
                 self.last_map = None
-                self.last_update = self.last_10_maps[-1][2]
+                self.last_update = self.last_maps[-1][2]
         else:
-            self.last_10_maps = []
+            self.last_maps = []
             self.current_map = None
             self.last_update = int(time.time())
 
     def store(self):
 
         with open("./history.pickle", "wb") as historyPickleFile:
-            pickle.dump(self.last_10_maps, historyPickleFile)
+            pickle.dump(self.last_maps, historyPickleFile)
 
     def writeUpdate(self, map_name, players):
         assert map_name != self.current_map
         self.last_map = self.current_map
         self.current_map = map_name
         ctime = int(time.time())
-        self.last_10_maps.append((map_name,players,  ctime))
+        self.last_maps.append((map_name,players,  ctime))
         self.store()
 
     def query(self, serveraddress: tuple):
@@ -55,20 +55,20 @@ class History(object):
         return self.last_map
 
     def getLast10Maps(self):
-        return self.last_10_maps
+        return self.last_maps
 
     def getFormattedCurrentMap(self):
-        return f"`{self.current_map}` with `{self.last_10_maps[-1][1]}` players - since: <t:{self.last_10_maps[-1][2]}:R>"
+        return f"`{self.current_map}` with `{self.last_maps[-1][1]}` players - since: <t:{self.last_maps[-1][2]}:R>"
 
     def getFormattedLastMap(self):
         try:
-            return f"`{self.last_map}` with `{self.last_10_maps[-2][1]}` players - since: <t:{self.last_10_maps[-2][2]}:R>"
+            return f"`{self.last_map}` with `{self.last_maps[-2][1]}` players - since: <t:{self.last_maps[-2][2]}:R>"
         except:
             return "`No previous map`"
         
     def getFormattedLast10Maps(self):
         formattedText = ""
-        for map_name, players, timestamp in self.last_10_maps:
+        for map_name, players, timestamp in reversed(self.last_maps[-10:]):
             formattedText += ((f"`{map_name}` with `{players}` players - since: <t:{timestamp}:R>\n"))
         return formattedText
 
